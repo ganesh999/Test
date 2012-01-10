0fadf566bf94aa8b7f9e1feaa851505eb02bf7dd
ganesh999 <ganesh.gk@xminds.com>
2012-01-10 13:47:46 +0530

    Changed Server Name

--- Deploy/Capistrano/config/deploy/staging.rb
+++ Deploy/Capistrano/config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :deploy_to, "$HOME/staging"
 
-role :app, "APP_DOMAIN eg:wwws.example.com"
+role :app, "localhost"
 #role :db, "fms.xminds.com", :primary => true
 
 # Email notifications
@@ -10,7 +10,7 @@ set :notify_emails, ["admin@xminds.com" ]
 Notifier.configure do |config|
   config[:recipient_addresses] = notify_emails
   config[:subject_prepend] = "[DEPLOY]"
-  config[:site_name] = "APP_DOMAIN eg:wwws.example.com"
+  config[:site_name] = "localhost"
   config[:email_content_type] = "text/html" # OR "text/plain" if you want the plain text version of the email
   config[:sections] = %w(deployment release_data source_control latest_release previous_release)
 end
